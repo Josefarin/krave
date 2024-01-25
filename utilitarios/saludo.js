dc05bd5d52775eda587058971b045b9501b04dde
Josefarin <josfeboer@gmail.com>
2024-01-25 01:03:49 -0500

    utilitarios

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,10 +1,10 @@
-salud=function(){
-    let nombre=recuperarTexto("txnom");
-    let apellido=recuperarTexto("txape");
-}
-recuperarTexto=function(idComponente){
-let comp; let valor;
-comp=document.getElementById(idComponente);
-valorIgresado=comp.value;
-return valorIgresado;
+salud = function() {
+    let nombre = recuperarTexto("txnom");
+    let apellido = recuperarTexto("txape");
+    let edad = recuperarTexto("txeda");
+    let altura = recuperarTexto("txesta");
+    let mensajeBienvenida = "Bienvenido " + nombre + " " + apellido;
+    mostrarTexto("lbresultados", mensajeBienvenida);
+    mostrarImagen("imsalud", "./img/0.gif");
+    mostrarTextoCaja("txnom", "");
 }
