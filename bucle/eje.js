e17a0810c0fb804eda6c6a7b9ec1730995863f20
Josefarin <josfeboer@gmail.com>
2024-02-05 09:29:59 -0500

    bucle

---
+++ bucle/eje.js
@@ -0,0 +1,35 @@
+mostrarNumeros=function(){
+    console.log("antesv del for");
+    for(let i=0;1<4;i++){
+        console.log(i);
+    }
+    console.log("despues");
+}
+mostrarNumeros2=function(){
+    console.log("antesv del for");
+    for(let indi=1;indi<=5;indi++){
+        console.log(indi);
+    }
+    console.log("despues");
+}
+mospares=function(){
+    console.log("antesv del for");
+    for(let x=2;x<=10;x+=2){
+        console.log(x);
+    }
+    console.log("despues");
+}
+mosimpa=function(){
+    console.log("antesv del for");
+    for(let i=10;i>=0;i--){
+        console.log(i);
+    }
+    console.log("despues");
+}
+mosimpar=function(){
+    console.log("antesv del for");
+    for(let a=1;a<=21;a+=2){
+        console.log(a);
+    }
+    console.log("despues");
+}
